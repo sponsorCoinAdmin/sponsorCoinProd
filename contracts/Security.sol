ac360633378a2c14ee4dd081f3a1ecc8cb89149a
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2023-02-04 16:04:06 -0500

    Add Hardhat for testing reasons

--- contracts/Security.sol
+++ contracts/Security.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
+import "hardhat/console.sol";
+
+
 contract Security {
     address private  rootAdmin;
  
     constructor()  {
         rootAdmin = msg.sender;
+        console.log("HARDHAT LOGGING WORKS");
     }
 
     modifier onlyRootAdmin () {
         require (msg.sender == rootAdmin, "Security Access Violation");
         _;
     }
-
 }
