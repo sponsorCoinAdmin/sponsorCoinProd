994702579a3bd33ad415376425ee602562226fbb
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2023-03-22 22:44:57 -0400

    WIP

--- test/prod/lib/spCoinReadMethods.js
+++ test/prod/lib/spCoinReadMethods.js
@@ -4,23 +4,6 @@ let spCoinContractDeployed;
 
 //////////////////////////// ROOT LEVEL FUNCTIONS ////////////////////////////
 
-addPatreonSponsors = async (_accountKey, _accountSponsorKeys) => {
-  logFunctionHeader(
-    "addPatreonSponsors = async(" + _accountKey + ", " + _accountSponsorKeys + ")"
-  );
-
-  logDetail("JS => For Account[" + _accountKey + "]: " + _accountKey + ")");
-  logDetail("JS => Adding " + _accountSponsorKeys.length + " Sponsors To Blockchain Network"
-  );
-
-  let sponsorCount = 0;
-  for (sponsorCount; sponsorCount < _accountSponsorKeys.length; sponsorCount++) {
-    let _sponsorKey = _accountSponsorKeys[sponsorCount];
-    await addPatreonSponsor(_accountKey, _sponsorKey);
-  }
-  logDetail("JS => Inserted = " + sponsorCount + " Sponsor Records");
-  return --sponsorCount;
-};
 
 setCreateContract = (_spCoinContractDeployed) => {
   spCoinContractDeployed = _spCoinContractDeployed;
@@ -106,19 +89,6 @@ getAccountAgentKeys = async (_accountKey) => {
 
 /////////////////////// SPONSOR RECORD FUNCTIONS ///////////////////////
 
-addPatreonSponsor = async (_accountKey, _sponsorKey) => {
-  logFunctionHeader(
-    "addPatreonSponsor = async(" + _accountKey + ", " + _sponsorKey + ")"
-  );
-
-  logDetail("JS => For Account[" + _accountKey + "]: " + _accountKey + ")");
-  logDetail("JS => Inserting " + _sponsorKey + " Sponsors To Blockchain Network"
-  );
-
-  logDetail("JS => Inserting Sponsor " + _sponsorKey );
-  await spCoinContractDeployed.addPatreonSponsor(_accountKey, _sponsorKey);
-};
-
 getAgentRecordKeys = async (_accountKey, _sponsorAccountKey) => {
   logFunctionHeader("getAgentRecordKeys = async(" + _accountKey + ", " + _sponsorAccountKey + ")" );
   let maxSize = await spCoinContractDeployed.getAgentRecordKeySize(_accountKey, _sponsorAccountKey);
@@ -143,46 +113,11 @@ getAgentRecordKeySize = async (_accountKey, _sponsorAccountKey) => {
 
 /////////////////////// AGENT RECORD FUNCTIONS ////////////////////////
 
-addSponsorAgent = async (_accountKey, _sponsorAccountKey, _accountAgentKey) => {
-  logFunctionHeader(
-    "accountAgentKey = async(" + _accountKey + ", " + _sponsorAccountKey + ", " + _accountAgentKey + ")"
-  );
-  logDetail("JS => For Account[" + _accountKey + "]: " + _accountKey + ")");
-  logDetail("JS => Adding Agent " + _accountAgentKey + " To Blockchain Network");
 
-  logDetail("JS =>  " + _accountKey + ". " + "Inserting Agent[" + _accountKey + "]: " + _accountAgentKey );
-  await spCoinContractDeployed.addSponsorAgent( _accountKey, _sponsorAccountKey, _accountAgentKey );
-  logDetail("JS => "+ "Added Agent " + _accountAgentKey + " Record to SponsorKey " + _sponsorAccountKey);
-};
-
-addSponsorAgents = async (_accountKey, _sponsorAccountKey, _accountAgentKeys) => {
-  logFunctionHeader(
-    "addSponsorAgents = async(" + _accountKey + ", " + _sponsorAccountKey + ", " + _accountAgentKeys + ")"
-  );
-  logDetail("JS => For Account[" + _accountKey + "]: " + _accountKey + ")");
-  logDetail("JS => For Sponsor[" + _sponsorAccountKey + "]: " + _sponsorAccountKey + ")");
-  logDetail("JS => Inserting " + _accountAgentKeys.length + " Agents To Blockchain Network"
-  );
-  logDetail("JS => _accountAgentKeys = " + _accountAgentKeys);
-
-  let agentSize = _accountAgentKeys.length;
-  logDetail("JS => agentSize.length = " + agentSize);
-  let agentCount = 0;
-  for (let agentCount = 0; agentCount < agentSize; agentCount++) {
-    let agentAccountKey = _accountAgentKeys[agentCount];
-    logDetail("JS =>  " + agentCount + ". " + "Inserting Agent[" + agentCount + "]: " + agentAccountKey );
-    await addSponsorAgent( _accountKey, _sponsorAccountKey, agentAccountKey );
-  }
-  logDetail("JS => "+ "Inserted = " + agentSize + " Agent Records");
-  return agentCount;
-};
 
 /////////////////////// EXPORT MODULE FUNCTIONS ///////////////////////
 
 module.exports = {
-  addPatreonSponsors,
-  addSponsorAgent,
-  addSponsorAgents,
   getAccountAgentKeys,
   getAccountAgentKeySize,
   getAccountKeys,
