54aa0b375a57b3470a5b9584013399f9c32b86f1
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2023-05-03 04:04:19 -0400

    Fix UINT256 precision

--- prod/lib/utils/serialize.js
+++ prod/lib/utils/serialize.js
@@ -39,7 +39,7 @@ deSerializedAccountRec = async (serializedAccountRec) => {
   logExitFunction();
   return accountStruct;
 };
-const hexToDecimal = hex => parseInt(hex, 16);
+const hexToDecimal = hex => { let dec = BigInt(hex); return dec.toString(10) };
 const bigIntToDecimal = bigInt => parseInt(bigInt).toLocaleString('fullwide', {useGrouping:false});
 // parseInt(bigInt, 10);
 
@@ -52,15 +52,12 @@ addAccountField = (key, value, accountStruct) => {
       break;
     case "balanceOf":
       console.log("balanceOf: " + value + "," + accountStruct.balance)
-      // convert("115792089237316195423570985008687907853269984665640564039457584007913129639935");
-      //convert("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
-      // let dec = h2d("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
 
-      // console.log("ZZZZZ DEC = " + dec);
-      // dec = h2d(value);
-      console.log("ZZZZZ value", value, "= DEC", + h2d(value));
+      console.log("PPPP value", "0xf000000039fd6e51aad88f6f4ce6ab8827279cfffb92266", "= DEC", hexToDecimal("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"));
+      dec = hexToDecimal("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
+      console.log( dec);
 
-      accountStruct.balanceOf = bigIntToDecimal(value);
+      accountStruct.balanceOf = hexToDecimal(value);
     break;
     case "stakedSPCoins":
       accountStruct.stakedSPCoins = hexToDecimal(value);
@@ -115,112 +112,6 @@ parseAddressStrRecord = (strRecord) => {
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function h2d(s) {
-
-  function add(x, y) {
-      var c = 0, r = [];
-      var x = x.split('').map(Number);
-      var y = y.split('').map(Number);
-      while(x.length || y.length) {
-          var s = (x.pop() || 0) + (y.pop() || 0) + c;
-          r.unshift(s < 10 ? s : s - 10); 
-          c = s < 10 ? 0 : 1;
-      }
-      if(c) r.unshift(c);
-      return r.join('');
-  }
-
-  var dec = '0';
-  s.split('').forEach(function(chr) {
-    console.log("S=",s,"chr=",chr);
-      var n = parseInt(chr, 16);
-      for(var t = 8; t; t >>= 1) {
-          dec = add(dec, dec);
-          if(n & t) dec = add(dec, '1');
-      }
-  });
-  return dec;
-}
-
-function convert(hex) {
-  console.log(">>>>>>>>>>>>>>converting hex: " + hex);
-//  var hex = document.calcform.x.value;
-//  hex = hex.replace("0x","");
-//  hex = hex.replace("0X","");
-//  try {
-  //  var x = new BigNumber(hex, 16);
-   console.log(">>>>>>>>>>>>>>converting BigNumber.from(hex).toString()   : " + BigNumber.from(hex).toString());
-   console.log(">>>>>>>>>>>>>>converting BigNumber.from(hex).toHexString(): " + BigNumber.from(hex).toHexString());
-   console.log("=====================================================");
-
-//    xelem.style.backgroundColor = "white";
-//  }
-//  catch(err) {
-//    xelem.style.backgroundColor = "#fff0f0";
-//    return;
-//  }
-/*
- var xx=x.toString(10);
-//  yelem.value = xx;
- y3elem = x.toString(2);
- if( x.isInt() && x.gte(0) ) {
-   if( hex.length==2 && x.gte("80", 16) ) { x=x.minus("100",16); }
-   if( hex.length==4 && x.gte("8000", 16) ) { x=x.minus("10000",16); }
-   if( hex.length==8 && x.gte("80000000", 16) ) { x=x.minus("100000000",16); }
-   var t1 = new BigNumber("8000000000000000",16);
-   var t2 = new BigNumber("10000000000000000",16);
-   if( hex.length==16 && x.gte(t1) ) { x=x.minus(t2); }
-   if( hex.length==2 || hex.length==4 || hex.length==8 || hex.length==16 )
-     y2elem.value = x.toString(10);
-   else
-     y2elem.value = "N/A";
- }
- else
-   y2elem.value = "N/A";
-  //  var yd=yelem.value.match(/\d/g);
-  var y2d=y2elem.value.match(/\d/g);
-  var y3d=y3elem.match(/\d/g);
-  //  var ylabel="Decimal number";
-   var y2label="Decimal from signed 2's complement";
-   var y3label="Binary number";
-  //  if( yd!=null ) ylabel+=" ("+yd.length+((yd.length==1)?" digit)":" digits)");
-   if( y2d!=null ) y2label+=" ("+y2d.length+((y2d.length==1)?" digit)":" digits)");
-   if( y3d!=null ) y3label+=" ("+y3d.length+((y3d.length==1)?" digit)":" digits)");
-//  document.getElementById("ylabel").innerHTML = ylabel;
-//  document.getElementById("y2label").innerHTML = y2label;
-//  document.getElementById("y3label").innerHTML = y3label;
- hex=hex.toUpperCase();
- var txt="("+hex+")\u2081\u2086 = ";
- var d,e,minus=false;
- var len=hex.length;
- if( hex[0]=="-" ) { txt+="-["; hex=hex.substr(1); len--; minus=true; }
-//  var idot=hex.indexOf(".");
- if( idot>=0 ) { hex=hex.substring(0,idot)+hex.substring(idot+1,len); len--; }
-//  else idot=len;
-//  etbl = ["\u2070","\u00B9","\u00B2","\u00B3","\u2074","\u2075","\u2076","\u2077","\u2078","\u2079"];
- for(var i=0; i<len; i++) {
-   d = hex.charCodeAt(i);
-   if( d<58 ) d-=48;
-   else if( d>64 ) d-=55;
-   //e = len-i-1;
-   e = idot-i-1;
-   e=e.toString();
-   txt+="("+d+" \u00D7 16";
-   for(var k=0; k<e.length; k++)
-     if( e[k]=="-" )
-       txt+="\u207B";
-     else
-       txt+=etbl[e[k]];
-   txt+=")";
-   if( i<len-1 ) txt+=" + ";
- }
- if( minus ) txt+="]";
- txt+=" = ("+xx+")\u2081\u2080";
- document.getElementById("y4").value = txt;
- */
-}
-
 
 module.exports = {
   addAccountField,
