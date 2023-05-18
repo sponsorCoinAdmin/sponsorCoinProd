6256593313acd34fc461ce356e44ba2eed5748a8
Robin Melanson <robin.e.melanson@gmail.com>
2023-05-18 01:25:48 -0400

    Final commit before reverting to my PC

--- contracts/utils/TimeUtils.sol
+++ contracts/utils/TimeUtils.sol
@@ -46,31 +46,3 @@ contract TimeUtils {
         return millenniumDivisor;
     }
 }
-
-/*
-contract Countdown {
-
-    function getTimeUnits() external view returns(uint, uint, uint, uint, uint, uint) {
-        return(block.timestamp, 1 seconds, 1 minutes, 1 hours, 1 days, 1 weeks);
-    }
-
-
-    uint yesVote;
-    uint noVote;
-
-    uint public startTime;
-    function start() external {
-        startTime = block.timestamp;
-    }
-
-    function voteYes() external {
-        require(block.timestamp < startTime + 60 seconds, "voting period passed, wait next year");
-        yesVote++;
-    }
-
-    function voteNo() external {
-        require(block.timestamp < startTime + 60 seconds, "voting period passed, wait next year");
-        noVote++;
-    }
-}
-*/
