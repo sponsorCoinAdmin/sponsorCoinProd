04895dfae0c84ef4b8ff62b3a3607ecd3989b42a
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-05-23 14:02:18 -0400

    WIP USE THIS WORKING VERSION

---
+++ contracts/accounts/StakingManager.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+/// @title ERC20 Contract
+import "./UnSubscribe.sol";
+
+contract StakingManager is UnSubscribe{
+    constructor(){
+    }
+    
+}
