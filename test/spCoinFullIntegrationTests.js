867ee04b7a140a241bbb4fae7b9cedca6d6a71f1
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-06-08 04:10:23 -0400

    WIP

--- test/spCoinFullIntegrationTests.js
+++ test/spCoinFullIntegrationTests.js
@@ -188,13 +188,12 @@ describe("spCoinContract", function () {
     "1.000000000000000008"
   );
   
-  //  await spCoinAddMethods.depositRecipientStakingRewards(
-  //   SPONSOR_ACCOUNT_KEYS[0],
-  //   RECIPIENT_ACCOUNT_KEYS[1], 
-  //   RECIPIENT_RATES[4],
-  //   333
-  // );
-
+   await spCoinAddMethods.depositRecipientStakingRewards(
+    SPONSOR_ACCOUNT_KEYS[0],
+    RECIPIENT_ACCOUNT_KEYS[1], 
+    RECIPIENT_RATES[4],
+    333
+  );
 
   await spCoinAddMethods.depositAgentStakingRewards(
     RECIPIENT_ACCOUNT_KEYS[1],
