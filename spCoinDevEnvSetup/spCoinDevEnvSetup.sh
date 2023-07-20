7b57757508a754ac4ee71a1472635e303ae43637
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2023-07-20 00:38:42 -0400

    Update spCoinDevEnvSetup.sh

--- spCoinDevEnvSetup/spCoinDevEnvSetup.sh
+++ spCoinDevEnvSetup/spCoinDevEnvSetup.sh
@@ -1,19 +1,18 @@
 # Requires git setup
-git clone git@github.com:sponsorCoinAdmin/dev-contracts-SpCoin.git
-cp ../../dev-contracts-SpCoin/.env .
-
-// NOTE: This shell script must be run from this directory ./SPCoinDevSetup
-
-export SPONSOR_COIN_ENV=$PWD
-cd ..
+export SPONSOR_COIN_DIR=dev-contracts-spcoin;
+git clone git@github.com:sponsorCoinAdmin/$SPONSOR_COIN_DIR.git
+cd $SPONSOR_COIN_DIR
 export SPONSOR_COIN_HOME=$PWD
+cd ..
+export SPONSOR_COIN_ENV=$PWD/$SPONSOR_COIN_DIR
+
+// NOTE: This shell script must be run from this directory ./SpCoinDevSetup
+
+
 echo "export SPONSOR_COIN_HOME=$SPONSOR_COIN_HOME" | tee -a ~/.bashrc
 cd SPCoinDevEnvSetup
 echo "export SPONSOR_COIN_ENV=$SPONSOR_COIN_ENV" | tee -a ~/.bashrc
 
 echo ". "$SPONSOR_COIN_ENV"/.e" | tee -a ~/.bashrc
 
-set -o vi
-. $SPONSOR_COIN_ENV/.e
-m
-cdcurr
+cp ./.env $SPONSOR_COIN_DIR
