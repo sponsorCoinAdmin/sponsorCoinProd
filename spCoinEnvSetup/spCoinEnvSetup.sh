1cdb6c846948959e28e4660c9346aa860feac9d7
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2023-07-20 04:10:21 -0400

    Update spCoinEnvSetup.sh

--- spCoinEnvSetup/spCoinEnvSetup.sh
+++ spCoinEnvSetup/spCoinEnvSetup.sh
@@ -2,10 +2,16 @@
 export SPONSOR_COIN_ROOT_DIR=$PWD
 export SPONSOR_COIN_DIR=dev-contracts-spcoin;
 export SPONSOR_COIN_DEV_DIR=spCoinEnvSetup;
+echo "DownLoading Sponsor Coin from gitHub"
 git clone git@github.com:sponsorCoinAdmin/$SPONSOR_COIN_DIR.git
+cd $SPONSOR_COIN_DIR
+echo "Installing the Node Libraries"
+npm i
+echo "Configuring SponsorCoin Environment"
 export SPONSOR_COIN_HOME=$SPONSOR_COIN_ROOT_DIR/$SPONSOR_COIN_DIR
 export SPONSOR_COIN_ENV=$SPONSOR_COIN_HOME/$SPONSOR_COIN_DEV_DIR
 
+echo "Adding startup configuration Files to bootstrap file ~/.baschrc"
 echo "export SPONSOR_COIN_HOME=$SPONSOR_COIN_HOME" | tee -a ~/.bashrc
 echo "export SPONSOR_COIN_ENV=$SPONSOR_COIN_ENV" | tee -a ~/.bashrc
 
